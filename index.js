57137c48e01de878aebc1343f5d3ac8eca5fd961
Kam Lasater <ckl@seekayel.com>
2023-06-21 21:06:35 -0400

    need to .default hmmm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const db = require('@cyclic.sh/dynamodb')
-
+const db = require('@cyclic.sh/dynamodb').default
+console.log(db)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
